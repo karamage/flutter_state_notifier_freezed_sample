baf18d7a5f3fbf6f007a0e660220ae97cf78884a
karamage <karamage.umauma@gmail.com>
2020-05-16 13:38:18 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:provider/provider.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StateNotifierProvider<CounterNotifier, CounterState>(
+        create: (_) => CounterNotifier(),
+        child: HomePage(),
+      ),
+    );
+  }
+}
+
+class CounterState {
+  CounterState(this.count);
+  int count = 0;
+}
+
+class CounterNotifier extends StateNotifier<CounterState> {
+  CounterNotifier() : super(CounterState(0));
+  void increment() {
+    state = CounterState(++state.count);
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minimum State Notifier Example'),
+      ),
+      body: Text(
+        context.select<CounterState, int>((state) => state.count).toString(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read<CounterNotifier>().increment(),
+      ),
+    );
+  }
+}
